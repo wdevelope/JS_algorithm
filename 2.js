f19a073230a425ff6ccf256e84583815e824b3ce
seongwong woo <dnqkr18@gmail.com>
2023-10-25 10:32:19 +0900

    2

---
+++ 2.js
@@ -0,0 +1,25 @@
+// 01. 배열의 평균값
+
+function solution(numbers) {
+  var answer = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return answer;
+}
+
+// 02. 편지
+
+function solution(message) {
+  const letterSize = 2;
+  const messageLength = message.length;
+  const paperWidth = messageLength * letterSize;
+
+  return paperWidth;
+}
+
+// 03. 피자 나눠 먹기 (1)
+
+function solution(n) {
+  const pizzaSlices = 7;
+  const pizzasNeeded = Math.ceil(n / pizzaSlices);
+
+  return pizzasNeeded;
+}
